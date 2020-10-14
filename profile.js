507a5017e61df12fe4f00d321fe12bd0d526af2c
ayeshani <ayeshanikaushalya@gmail.com>
2020-10-14 12:18:56 +0530

    --- Auto-Git Commit ---

--- profile.js
+++ profile.js
@@ -22,3 +22,24 @@ $(document).ready(function() {
        $(".file-upload").click();
     });
 });
+
+
+    $(document).ready(function(){
+        $('#but_add').click(function(){
+
+    // Create clone of <div class='input-form'>
+        var newel = $('.input-form:last').clone();
+
+    // Add after last <div class='input-form'>
+        $(newel).insertAfter(".input-form:last");
+     });
+
+    $('.txt').focus(function(){
+        $(this).css('border-color','red');
+    });
+
+    $('.txt').focusout(function(){
+        $(this).css('border-color','initial');
+    });
+
+    });
